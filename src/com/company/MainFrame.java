d5efe86ae7d4dc8ee245dc28fed9486a380de495
Salim-AFK5 <74103946+Salim-AFK5@users.noreply.github.com>
2023-03-03 15:25:47 +0600

    Updated

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -1,36 +1,46 @@
 package com.company;
 
-        import java.awt.*;
-        import java.awt.event.*;
-        import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class MainFrame extends JFrame{
-    private JButton button = new JButton("Превернуть");
+    private JButton button = new JButton("OK");
 
-    private JTextField input = new JTextField("", 5);
-    private JLabel lable = new JLabel("Ваш текст: ");
+    private JTextField input = new JTextField("", 2);
+    private JLabel inputLabel = new JLabel("Input:");
+
+    private JTextField output = new JTextField("", 2);
+    private JLabel outputLabel = new JLabel("Output:");
 
     public MainFrame (){
-        super("Переворачиватель текста");
-        this.setBounds(100,200,350,80);
+        super("String");
+        this.setBounds(200,200,400,100);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container container = this.getContentPane();
-        container.setLayout(new GridLayout(1,2,5, 5));
-        container.add(lable);
+        container.setLayout(new GridLayout(2,3,5, 5));
+        container.add(inputLabel);
         container.add(input);
         ButtonGroup group = new ButtonGroup();
         button.addActionListener(new ButtonEventListener());
         container.add(button);
+        container.add(outputLabel);
+        container.add(output);
+
     }
 
     class ButtonEventListener implements ActionListener{
         public void actionPerformed(ActionEvent e){
             String inputText = input.getText();
-            StringBuilder outputText = new StringBuilder();
-            for (int i = inputText.length() - 1; i >= 0; i--) {
-                outputText.append(inputText.charAt(i));
+            StringBuilder outputText = new StringBuilder(inputText);
+            int length = outputText.length();
+            int startIndex = length % 2 == 0 ? length / 2 - 1 : length / 2;
+            outputText = new StringBuilder(outputText.substring(startIndex));
+            for (int i = 2; i < outputText.length(); i += 2) {
+                outputText.deleteCharAt(i);
             }
-            input.setText(outputText.toString());
+            output.setText(outputText.toString());
         }
     }
+
 }
