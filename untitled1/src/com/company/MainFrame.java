fbe86684605da0aa5b8657fb299b8771ee776235
Salim-AFK5 <74103946+Salim-AFK5@users.noreply.github.com>
2023-03-03 15:35:02 +0600

    Updated

---
+++ untitled1/src/com/company/MainFrame.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainFrame extends JFrame{
+    private JButton button = new JButton("OK");
+
+    private JTextField input = new JTextField("", 2);
+    private JLabel inputLabel = new JLabel("Input:");
+
+    private JTextField output = new JTextField("", 2);
+    private JLabel outputLabel = new JLabel("Output:");
+
+    public MainFrame (){
+        super("String");
+        this.setBounds(200,200,400,100);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container container = this.getContentPane();
+        container.setLayout(new GridLayout(2,3,5, 5));
+        container.add(inputLabel);
+        container.add(input);
+        ButtonGroup group = new ButtonGroup();
+        button.addActionListener(new ButtonEventListener());
+        container.add(button);
+        container.add(outputLabel);
+        container.add(output);
+
+    }
+
+    class ButtonEventListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            String inputText = input.getText();
+            StringBuilder outputText = new StringBuilder(inputText);
+            int length = outputText.length();
+            int startIndex = length % 2 == 0 ? length / 2 - 1 : length / 2;
+            outputText = new StringBuilder(outputText.substring(startIndex));
+            for (int i = 2; i < outputText.length(); i += 2) {
+                outputText.deleteCharAt(i);
+            }
+            output.setText(outputText.toString());
+        }
+    }
+
+}
